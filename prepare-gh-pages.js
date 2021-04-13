0b883b9fa9c8f7cfa04469e68970ba0163b46578
Adam Comella <rigdern@gmail.com>
2021-04-13 15:23:51 -0700

    Add .nojekyll file so GitHub Pages will serve the _output/ directory

--- prepare-gh-pages.js
+++ prepare-gh-pages.js
@@ -9,5 +9,10 @@ function copyFile(from, to) {
   fs.copyFileSync(from, to);
 }
 
+// The .nojekyll file tells GitHub Pages it's okay to server directories that
+// begin with an underscore. 
+// For details see https://github.blog/2009-12-29-bypassing-jekyll-on-github-pages/
+fs.writeFileSync('./_gh-pages/.nojekyll', '');
+
 copyFile('./index.html', './_gh-pages/index.html');
 copyFile('./_output/main.js', './_gh-pages/_output/main.js');
